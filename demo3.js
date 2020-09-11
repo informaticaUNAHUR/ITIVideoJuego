28badeb171d0f57fcbbc230346b9a212f89dca53
informaticaUNAHUR <39915206+informaticaUNAHUR@users.noreply.github.com>
2020-09-11 16:54:33 -0300

    Create demo3.js

---
+++ demo3.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#example').DataTable( {
+        dom: 'Bfrtip',
+        columnDefs: [
+            {
+                targets: 1,
+                className: 'noVis'
+            }
+        ],
+        buttons: [
+            {
+                extend: 'colvis',
+                columns: ':not(.noVis)'
+            }
+        ]
+    } );
+} );
