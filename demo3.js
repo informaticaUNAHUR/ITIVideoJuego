a434ed7250c4cdb98915db34388fa5dcea166f77
informaticaUNAHUR <39915206+informaticaUNAHUR@users.noreply.github.com>
2020-09-11 17:04:54 -0300

    Update demo3.js

--- demo3.js
+++ demo3.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+    var table = $('#example').DataTable();
+ 
+    $('#example tbody').on( 'click', 'tr', function () {
+        $(this).toggleClass('selected');
+    } );
+ 
+    $('#button').click( function () {
+        alert( table.rows('.selected').data().length +' row(s) selected' );
+        table( {
+            select: true
+        } );
+    } );
+    
     $('#example').DataTable( {
         dom: 'Bfrtip',
         columnDefs: [
